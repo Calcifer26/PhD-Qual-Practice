775002e623b55a45e92b68220a7d5cbd098c53c3
akg92 <shibintv@hotmail.com>
2020-04-17 16:17:09 -0500

    add mailer test config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,10 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  #Mailer action
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => "test.yourhost.com" }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
