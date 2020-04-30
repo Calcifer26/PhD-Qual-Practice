cf6325a026555b0c31496fede2572aa47f8d10e5
akg92 <shibintv@hotmail.com>
2020-04-30 13:03:01 -0500

    fixing fb auth confirmation

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,15 +2,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def facebook
       # You need to implement the method below in your model (e.g. app/models/user.rb)
       @user = User.from_omniauth(request.env["omniauth.auth"])
-  
+      debugger
       if @user.persisted?
         sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
         set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
       else
         # save the current omniauth info
-        @user.save
         @user.is_admin = false
         @user.skip_confirmation!
+        @user.save
         # @user.confirm!
         session["devise.auth_data"] = request.env["omniauth.auth"]
         sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
